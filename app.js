251c01694d9a4fe4575f127f89bec730894d4441
Jeffrey <aligula.jeff@gmail.com>
2023-03-10 19:35:17 +0300

    updated cors

--- app.js
+++ app.js
@@ -4,20 +4,21 @@ dotenv.config();
 const app = express();
 import morgan from "morgan";
 import bodyParser from "body-parser";
+import cors from "cors"
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
 
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
-    return res.status(200).json({});
-  }
-  next();
-});
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
 
 import userRoutes from "./api/routes/users.js";
 import patientRoutes from "./api/routes/patients.js";
@@ -26,6 +27,7 @@ import doctorsRoutes from "./api/routes/doctors.js";
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 //Routes used to handle the requests
 app.use("/users", userRoutes);
