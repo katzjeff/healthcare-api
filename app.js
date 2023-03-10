3c4f65487794da0e8366b92faf49b8ff2484b202
Jeffrey <aligula.jeff@gmail.com>
2023-03-10 15:10:45 +0300

    updated with error handling

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
-// require("dotenv").config();
+import morgan from "morgan";
+
 import userRoutes from "./api/routes/users.js";
 import patientRoutes from "./api/routes/patients.js";
 
-//Middleware
+app.use(morgan("dev"));
+
+//Routes used to handle the requests
 app.use("/users", userRoutes);
 app.use("/patients", patientRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Resource Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 export default app;
